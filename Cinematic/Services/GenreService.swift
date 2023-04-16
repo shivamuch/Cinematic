8cafb831ba2394e06b2014878af39ec065553d29
Shivam Bajaj <shivam.bajaj1609@gmail.com>
2023-04-16 00:11:39 -0400

    Service classes created

---
+++ Cinematic/Services/GenreService.swift
@@ -0,0 +1,8 @@
+//
+//  GenreService.swift
+//  Cinematic
+//
+//  Created by Shivam Bajaj on 2023-04-16.
+//
+
+import Foundation
---
+++ Cinematic/Services/Service.swift
@@ -0,0 +1,8 @@
+//
+//  Service.swift
+//  Cinematic
+//
+//  Created by Shivam Bajaj on 2023-04-16.
+//
+
+import Foundation
