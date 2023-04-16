82faeb0158ffacd87a9d5683a1ad972384afb46f
Shivam Bajaj <shivam.bajaj1609@gmail.com>
2023-04-16 11:02:09 -0400

    Added the API User Interface to the application

---
+++ Cinematic/Constants.swift
@@ -0,0 +1,26 @@
+//
+//  Constants.swift
+//  Cinematic
+//
+//  Created by Shivam Bajaj on 2023-04-15.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    struct coreData {
+        let movie = "CoreDataMovie"
+    }
+
+    struct api {
+        let key = "ddd54788bcb1b4f089ae69347d51b4c6"
+        let language = "pt-BR"
+        let url = "https://api.themoviedb.org/3"
+    }
+
+    struct color {
+        let darkGreen = UIColor(rgb: 0x081C25)
+        let lightGreen = UIColor(rgb: 0x02D475)
+    }
+}
