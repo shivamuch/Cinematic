ee61df38060773f26e202d72ab0c119301d0e152
Shivam Bajaj <shivam.bajaj1609@gmail.com>
2023-04-16 16:15:37 -0400

    Services for Genre and movie

--- Cinematic/Services/GenreService.swift
+++ Cinematic/Services/GenreService.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+struct Genres: Codable {
+    var genres: [Genre] = []
+}
+
+class GenreService: Service {
+
+    // MARK: - Requests
+
+    func fetchGenres(with
+        completion: @escaping ([Genre]?, GenreServiceError?) -> ()) {
+
+        let path = "/genre/movie/list"
+
+        guard let url = createApiUrl(with: path, queryItems: []) else { return }
+        URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) -> () in
+            do {
+                if error != nil {
+                    completion(nil, GenreServiceError.CannotFetch())
+                    return
+                }
+                guard let data = data else {
+                    completion(nil, GenreServiceError.CannotFetch())
+                    return
+                }
+
+                let result = try JSONDecoder().decode(Genres.self, from: data)
+                completion(result.genres, nil)
+            } catch {
+                completion(nil, GenreServiceError.CannotFetch())
+            }
+        }).resume()
+    }
+}
+
+// MARK: - Request errors
+
+enum GenreServiceError: Equatable, Error {
+  case CannotFetch(String = "not able to fetch" +
+                            "internet connection is not available")
+}
+
--- Cinematic/Services/MovieService.swift
+++ Cinematic/Services/MovieService.swift
@@ -6,3 +6,114 @@
 //
 
 import Foundation
+class MovieService: Service {
+
+    // MARK: - Requests
+
+    func fetchPopularMovies(with
+        page: Int,
+        completion: @escaping (MovieViewModel?, MovieServiceError?) -> ()) {
+
+        let query = [
+            URLQueryItem(name: "page", value: "\(page)")
+        ]
+
+        let path = "/movie/popular"
+
+        guard let url = createApiUrl(with: path, queryItems: query) else { return }
+        URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) -> () in
+            do {
+                if error != nil {
+                    completion(nil, MovieServiceError.CannotFetch())
+                    return
+                }
+                guard let data = data else {
+                    completion(nil, MovieServiceError.CannotFetch())
+                    return
+                }
+
+                let decoder = JSONDecoder()
+                decoder.setCustomDateDecodingStrategy()
+
+                let result = try decoder.decode(MovieViewModel.self, from: data)
+                completion(result, nil)
+            } catch {
+                completion(nil, MovieServiceError.CannotFetch())
+            }
+        }).resume()
+    }
+
+    func fetchMovie(with
+        id: Int,
+        completion: @escaping (Movie?, MovieServiceError?) -> ()) {
+
+        let path = "/movie/\(id)"
+
+        guard let url = createApiUrl(with: path, queryItems: []) else { return }
+        URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) -> () in
+            do {
+                if error != nil {
+                    completion(nil, MovieServiceError.CannotFetchMovie())
+                    return
+                }
+                guard let data = data else {
+                    completion(nil, MovieServiceError.CannotFetchMovie())
+                    return
+                }
+
+                let decoder = JSONDecoder()
+                decoder.setCustomDateDecodingStrategy()
+
+                let result = try decoder.decode(Movie.self, from: data)
+                completion(result, nil)
+            } catch {
+                completion(nil, MovieServiceError.CannotFetchMovie())
+            }
+        }).resume()
+    }
+
+    func fetchMovies(with
+        search: String,
+        page: Int,
+        completion: @escaping (MovieViewModel?, MovieServiceError?) -> ()) {
+
+        let query = [
+            URLQueryItem(name: "page", value: "\(page)"),
+            URLQueryItem(name: "query", value: search)
+        ]
+
+        let path = "/search/movie"
+
+        guard let url = createApiUrl(with: path, queryItems: query) else { return }
+        URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) -> () in
+            do {
+                if error != nil {
+                    completion(nil, MovieServiceError.CannotFetch())
+                    return
+                }
+          guard let data = data else {
+                    completion(nil, MovieServiceError.CannotFetch())
+                    return
+                }
+
+                let decoder = JSONDecoder()
+                decoder.setCustomDateDecodingStrategy()
+
+                let result = try decoder.decode(MovieViewModel.self, from: data)
+                completion(result, nil)
+            } catch {
+                completion(nil, MovieServiceError.CannotFetch())
+            }
+        }).resume()
+    }
+}
+
+// MARK: - Request errors
+
+enum MovieServiceError: Equatable, Error {
+    case CannotFetch(String = "Error while loading " +
+                              "verify internet connection.")
+    case CannotFetchMovie(String = "Error while loading" +
+                                   "Check your inteernet connection.")
+}
+
