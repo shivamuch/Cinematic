da08607e333f6da980bd11f7ff4aa6681fa2ea8e
Shivam Bajaj <shivam.bajaj1609@gmail.com>
2023-04-15 22:10:15 -0400

    Added new swift files for views of the application

---
+++ Cinematic/Views/Extensions/JSONDecoderExtension.swift
@@ -0,0 +1,8 @@
+//
+//  JSONDecoderExtension.swift
+//  Cinematic
+//
+//  Created by Shivam Bajaj on 2023-04-15.
+//
+
+import Foundation
---
+++ Cinematic/Views/MoviesView/CustomImageView.swift
@@ -0,0 +1,8 @@
+//
+//  CustomImageView.swift
+//  Cinematic
+//
+//  Created by Shivam Bajaj on 2023-04-15.
+//
+
+import Foundation
