abfda16866ab3714eae2e3e9e0ff3eddded85366
Shivam Bajaj <shivam.bajaj1609@gmail.com>
2023-04-16 16:13:30 -0400

    Added the Services for the API

--- Cinematic/Services/Service.swift
+++ Cinematic/Services/Service.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+class Service {
+
+    // MARK: - Utility
+    
+    func createApiUrl(with path:String, queryItems: [URLQueryItem]) -> URL? {
+        var query = [
+            URLQueryItem(name: "api_key", value: Constants.api().key),
+            URLQueryItem(name: "language", value: Constants.api().language)
+        ]
+        query.append(contentsOf: queryItems)
+        var urlComponents = URLComponents(string: "\(Constants.api().url)\(path)")
+        urlComponents?.queryItems = query
+        return urlComponents?.url
+    }
+}
+
