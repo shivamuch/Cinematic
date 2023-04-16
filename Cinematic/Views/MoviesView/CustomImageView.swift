4eb6ca4be85a6c9876161f9c59666d8cff7d5c4c
Shivam Bajaj <shivam.bajaj1609@gmail.com>
2023-04-15 23:06:41 -0400

    Update CustomImageView.swift
    Created the Custom Image view under the movies

--- Cinematic/Views/MoviesView/CustomImageView.swift
+++ Cinematic/Views/MoviesView/CustomImageView.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import UIKit
+let imageCache = NSCache<NSString, UIImage>()
+class CustomImageView: UIImageView {
+    private var imagePath = ""
+
+    func loadImageUsing(path: String) {
+
+        imagePath = path
+        guard let url = URL(string: path) else { return }
+
+        self.image = UIImage(named: "movie-cover")
+
+        if let imageFromCache = imageCache.object(forKey: path as NSString) {
+            self.image = imageFromCache
+            return
+        }
+
+        URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) -> () in
+            if error != nil { return }
+
+            guard let data = data else  { return }
+
+            DispatchQueue.main.async {
+                guard let imageToCache = UIImage(data: data) else { return }
+                if self.imagePath == path {
+                    self.image = imageToCache
+                }
+                imageCache.setObject(imageToCache, forKey: path as NSString)
+            }
+        }).resume()
+    }
+}
+
