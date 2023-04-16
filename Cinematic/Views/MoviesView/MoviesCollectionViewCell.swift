f35463e4502fd4c34abea1fab66c10904e739fc2
Shivam Bajaj <shivam.bajaj1609@gmail.com>
2023-04-15 23:05:56 -0400

    Worked on MoviesView & Movies collectionand with the storyboard

--- Cinematic/Views/MoviesView/MoviesCollectionViewCell.swift
+++ Cinematic/Views/MoviesView/MoviesCollectionViewCell.swift
@@ -5,4 +5,36 @@
 //  Created by Shivam Bajaj on 2023-04-15.
 //
 
-import Foundation
+import UIKit
+
+class MoviesCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var yearLabel: UILabel!
+    @IBOutlet private weak var posterImage: CustomImageView!
+
+    // MARK: - Object lifecycle
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        titleLabel.sizeToFit()
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        titleLabel.numberOfLines = 2
+    }
+
+    // MARK: - Setup
+
+    func setupCell(with movie: Movie) {
+        titleLabel.text = movie.title
+        yearLabel.text = movie.releaseYear
+        posterImage.loadImageUsing(path: movie.smallPosterPath)
+    }
+}
+
+    
+
+
