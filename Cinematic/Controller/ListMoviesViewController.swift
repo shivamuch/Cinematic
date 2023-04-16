b6c1ef275fb3a650b35a4e096d29d875d5024e92
Shivam Bajaj <shivam.bajaj1609@gmail.com>
2023-04-15 23:52:35 -0400

    Controller for search and listing movies with the screens

--- Cinematic/Controller/ListMoviesViewController.swift
+++ Cinematic/Controller/ListMoviesViewController.swift
@@ -5,4 +5,50 @@
 //  Created by Shivam Bajaj on 2023-04-15.
 //
 
-import Foundation
+import UIKit
+
+class ListMoviesViewController: UIViewController, MoviesViewInteractionLogic {
+
+    private var viewModel = ListMoviesViewModel()
+
+    @IBOutlet private weak var moviesView: MoviesView!
+    
+    // MARK: - Object lifecycle
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        title = viewModel.viewTitle
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        title = viewModel.viewTitle
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        setupSarchController()
+        fetchMovies()
+    }
+
+    // MARK: - Setup
+
+    private func setup() {
+        moviesView.viewController = self
+    }
+
+    private func setupSarchController() {
+        let searchMoviesViewController = SearchMoviesViewController(sender: self)
+        searchMoviesViewController.view.layoutIfNeeded()
+        let searchController = UISearchController(searchResultsController: searchMoviesViewController)
+        searchController.searchResultsUpdater = searchMoviesViewController
+        searchController.obscuresBackgroundDuringPresentation = true
+        searchController.searchBar.placeholder = "Nome do filme"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+    }
+
+}
+
+
