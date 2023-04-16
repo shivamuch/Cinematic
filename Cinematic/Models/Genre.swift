79a4715d8640bb6bc17cb5d070f2d50b08809a90
Shivam Bajaj <shivam.bajaj1609@gmail.com>
2023-04-15 22:04:34 -0400

    Models Created for the Movies and the Genre

---
+++ Cinematic/Models/Genre.swift
@@ -0,0 +1,14 @@
+//
+//  Genre.swift
+//  Cinematic
+//
+//  Created by Shivam Bajaj on 2023-04-15.
+//
+
+import Foundation
+
+struct Genre: Codable {
+    var id: Int = 0
+    var name: String = ""
+}
+
