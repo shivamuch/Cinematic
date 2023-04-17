7760bb287fe8004a065799adc41c65410709a06b
Shivam Bajaj <shivam.bajaj1609@gmail.com>
2023-04-17 00:00:08 -0400

    Worked on the Data source for the movies, to add poster, release date, title, vote of the movie

--- Cinematic/DataSource/MovieDataSource.swift
+++ Cinematic/DataSource/MovieDataSource.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import UIKit
+import CoreData
+class MovieDataSource {
+
+ 
+    func saveMovie(movie: Movie, completion: @escaping (Movie?, MovieDataSourceError?) -> ()) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            completion(nil, MovieDataSourceError.CannotSave())
+            return
+        }
+
+        let managedContext = appDelegate.container.viewContext
+
+        let newMovie = CoreDataMovie(context: managedContext)
+
+        newMovie.setValue(movie.id, forKey: "id")
+        newMovie.setValue(movie.overview, forKey: "overview")
+        newMovie.setValue(movie.posterPath, forKey: "posterPath")
+        newMovie.setValue(movie.releaseDate, forKey: "releaseDate")
+        newMovie.setValue(movie.title, forKey: "title")
+        newMovie.setValue(movie.voteAverage, forKey: "voteAverage")
+
+        do {
+            try managedContext.save()
+            completion(movie, nil)
+        } catch {
+            completion(nil, MovieDataSourceError.CannotSave())
+        }
+    }
+
+}
+
+
+
+
